7568143b6f70f77172d59b6f3bc5ca3ff1928b5f
computermouth <computermouth@crunchbangplusplus.org>
2018-06-20 23:14:03 -0700

    Statically link a C lib which is used by the binding C file

--- rust-to-c/build.rs
+++ rust-to-c/build.rs
@@ -1,7 +1,12 @@
 extern crate cc;
+extern crate pkg_config;
 
 fn main() {
     cc::Build::new()
         .file("src/double.c")
         .compile("libdouble.a");
+
+    pkg_config::Config::new().statik(true).probe("libconfuse").unwrap();
+
+//    println!("cargo:rustc-link-lib=static=confuse");
 }
--- rust-to-c/src/double.c
+++ rust-to-c/src/double.c
@@ -1,3 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <confuse.h>
+
 int double_input(int input) {
-    return input * 2;
+    cfg_t *cfg;
+
+    long int zero = 0;
+
+		cfg_opt_t opts[] = {
+			CFG_SIMPLE_INT("video_mode", &zero),
+			CFG_END()
+		};
+
+    cfg = cfg_init(opts, 0);
+    
+    cfg_free(cfg);
+
+    printf("howdy\n");
+    return pow(input * 2, 2);
 }
